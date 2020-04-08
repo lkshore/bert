62c29a2d93713f97e31647b0ef0ced3894c6fda6
zhanghao <zhanghaobuaa@gmail.com>
2020-04-08 18:56:38 -0400

    Add zsh wrapper for classifier and SQuAD tasks

---
+++ run_squad11.zsh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+export BERT_BASE_DIR=../checkpoints/uncased_L-12_H-768_A-12
+export SQUAD_DIR=../GLUE/SQuAD_v1.1/
+
+python run_squad.py \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --do_train=False \
+  --train_file=$SQUAD_DIR/train-v1.1.json \
+  --do_predict=True \
+  --predict_file=$SQUAD_DIR/dev-v1.1.json \
+  --train_batch_size=12 \
+  --learning_rate=3e-5 \
+  --num_train_epochs=2.0 \
+  --max_seq_length=384 \
+  --doc_stride=128 \
+  --output_dir=/tmp/squad_base/
---
+++ run_squad20.zsh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+export BERT_BASE_DIR=../checkpoints/uncased_L-12_H-768_A-12
+export SQUAD_DIR=../GLUE/SQuAD_v2.0/
+
+python run_squad.py \
+  --vocab_file=$BERT_LARGE_DIR/vocab.txt \
+  --bert_config_file=$BERT_LARGE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_LARGE_DIR/bert_model.ckpt \
+  --do_train=False \
+  --train_file=$SQUAD_DIR/train-v2.0.json \
+  --do_predict=True \
+  --predict_file=$SQUAD_DIR/dev-v2.0.json \
+  --train_batch_size=24 \
+  --learning_rate=3e-5 \
+  --num_train_epochs=2.0 \
+  --max_seq_length=384 \
+  --doc_stride=128 \
+  --output_dir=/tmp/squad_base/
+  --version_2_with_negative=True
+
